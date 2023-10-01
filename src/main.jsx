1bafeaf216395e20261d45a293d93d65334a3788
Praveen Singh <Praveensinghiics@gmail.com>
2023-10-01 13:19:26 +0530

    :bug: Fixed bug for production

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import "./index.css";
 
-import store from "./store";
+import store from "./Store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
